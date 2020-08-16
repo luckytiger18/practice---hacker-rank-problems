857d51a73431251dc2dd60838d712bc0fc5a178a
Annie Luc <lannie3186@gmail.com>
2020-08-16 13:54:32 -0700

    Made a list of intergers that includes words "Fizz", "Buzz", "FizzBuzz"

--- Hacker Rank/fizzBuzz.js
+++ Hacker Rank/fizzBuzz.js
@@ -2,17 +2,21 @@
 
 // But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
 
-function fizzBuzz(n) {  
-   for(var i =0; i < n; i++){
-       console.log(i)
-       if(i === 3/3){
-           console.log("divisible by 3")
-       }
-   }
+function fizzBuzz(n) {
+    var list = [""];
+    for (var i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            list.push("FizzBuzz")
+        } else if (i % 3 === 0) {
+            list.push("Fizz")
+        } else if (i % 5 === 0) {
+            list.push("Buzz")
+        } else {
+            list.push(i.toString())
+        }
+    }
+    return list
 
 };
 
-console.log(fizzBuzz(15))
-
-
-
+console.log(fizzBuzz(100))
