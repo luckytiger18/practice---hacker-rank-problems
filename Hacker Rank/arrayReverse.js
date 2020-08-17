0e5447cdd51b57eb89b4eefacc3bee9bc86a0fc6
Annie Luc <lannie3186@gmail.com>
2020-08-17 16:38:34 -0700

    -Practiced reverse an array and joining them with a space

---
+++ Hacker Rank/arrayReverse.js
@@ -0,0 +1,7 @@
+// Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+function wordReverse (arr){
+console.log(arr.reverse().join(" "))
+}
+
+console.log(wordReverse([1, 4, 3, 2]))
